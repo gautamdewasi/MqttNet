e6abdaed3e1ad1f3371a3b9b3697416a9d44ab26
gautamdewasi <47949558+gautamdewasi@users.noreply.github.com>
2020-10-01 11:56:04 +0530

    Update FileWriter.hpp

--- FileWriter.hpp
+++ FileWriter.hpp
@@ -1,10 +1,15 @@
+//....................................#ifndef preprocessor check for FILEWRITER_HPP macro, is it define or not.................................................. 
 #ifndef FILEWRITER_HPP
+//...................................if FILEWRITER_HPP macro is not define then #define preprocessor define it .................................................
 #define FILEWRITER_HPP
 
+//......................define  headers for using several function, which is included in these header files...............................
 #include <Arduino.h>
 #include <FS.h>
 
+// defining a class called FileWriter
 class FileWriter {
+ // using private keyword to define some members of class private, so that they doesnot access outside the class.
  private:
   File file_handle;
   char _filename[15];
@@ -15,7 +20,8 @@ class FileWriter {
   unsigned int received_size;
   const char *tmp_filename = "tmp";
   void parse_md5_stream(MD5Builder *md5, Stream *stream);
-
+ 
+ // deining some members of class public, so that they accessible outside the class using its OBJECTS
  public:
   FileWriter();
   bool Begin(const char *filename, const char *md5, size_t size);
@@ -29,4 +35,5 @@ class FileWriter {
   int GetPosition();
 };
 
+//.....................................................Undefining macro FILEWRITER_HPP ......................................
 #endif
